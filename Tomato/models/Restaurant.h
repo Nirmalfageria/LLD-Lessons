c85f7038325aab49d14a578e1f3c332dec69d05c
nirmal fageria <nirmalfageria15@gmail.com>
2025-08-23 15:00:18 +0530

    Add initial implementation of Restaurant class with menu management

---
+++ Tomato/models/Restaurant.h
@@ -0,0 +1,42 @@
+ #include<bits/stdc++.h>
+ using namespace std;
+ #include "MenuItems.h"
+
+ #define RESTAURANT_H
+ class Restaurant {
+    private:
+    static int nextId;
+    int id;
+    string name;
+    vector<MenuItem*> menu;
+    string location;
+    public:
+    Restaurant(string name, string location) : name(name), location(location) {
+        this->id = nextId++;
+    }       
+    ~Restaurant() {
+        for(auto item : menu) {
+            delete item;
+        }
+    }
+    int getId() const {
+        return id;
+    }
+    string getName() const {
+        return name;
+    }
+    string getLocation() const {
+        return location;
+    }
+    void addMenuItem(MenuItem* item) {
+        menu.push_back(item);
+    }
+    void displayMenu() const {
+        cout << "Menu for " << name << " :" << endl;
+        for(const auto& item : menu) {
+            item->display();
+        }
+    }   
+ };
+
+ Restaurant::nextId = 1;
